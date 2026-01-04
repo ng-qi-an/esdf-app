3a1a15346c0e24b7856ff67f3b58426efbd285ee
Ng Qi An <ng.qi.an@sji.edu.sg>
2026-01-04 21:50:06 +0800

    Started work on UI
    UI is web based, and the app is following a hybrid architecture (webview + python backend) to deliver customization and actual usability!

---
+++ main.py
@@ -0,0 +1,56 @@
+import multiprocessing
+import sys
+
+from PIL import Image, ImageDraw
+from pystray import Icon, Menu, MenuItem
+
+import webview
+
+# Adapted from pywebview pystray example
+if sys.platform == 'darwin':
+    ctx = multiprocessing.get_context('spawn')
+    Process = ctx.Process
+    Queue = ctx.Queue
+else:
+    Process = multiprocessing.Process
+    Queue = multiprocessing.Queue
+
+
+
+def create_image(width, height, color1, color2):
+    image = Image.new('RGB', (width, height), color1)
+    dc = ImageDraw.Draw(image)
+    dc.rectangle(
+        (width // 2, 0, width, height // 2),
+        fill=color2)
+    dc.rectangle(
+        (0, height // 2, width // 2, height),
+        fill=color2)
+    return image
+
+def create_settings():
+    window = webview.create_window(
+        title='ESDF Settings', 
+        url='index.html',
+        width=900,
+        height=600,
+        min_size=(400, 500)
+    )
+    webview.start(debug=True)
+
+webview_process = None
+
+def on_open():
+    global webview_process
+    webview_process = Process(target=create_settings)
+    if not webview_process.is_alive():
+        webview_process.start()
+
+def on_exit(icon, item):
+    icon.stop()
+
+if __name__ == '__main__':
+    menu = Menu(MenuItem('Open settings', on_open), MenuItem('Quit', on_exit))
+    icon = Icon('Pystray', create_image(64, 64, 'black', 'white'), menu=menu)
+    icon.run()
+    webview_process.terminate()
